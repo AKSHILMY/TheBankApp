935fc950aceb90d52b673e69e518bae91efa58a5
sohailparwany <77115237+ShanaazAhamed@users.noreply.github.com>
2022-07-15 10:00:15 +0530

    interest schedule

--- Web App/server/index.js
+++ Web App/server/index.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import customerRoutes from "./routes/customers.js";
 import agnetsRoutes from "./routes/agents.js";
+import cron from "node-cron";
+import { interest } from "./controllers/interestShedule.js";
 
 const app = express();
 const PORT = 5000;
@@ -16,6 +18,10 @@ app.get("/", (req, res) => res.send("Welcome to the Customers API!"));
 app.all("*", (req, res) =>
   res.send("You've tried reaching a route that doesn't exist.")
 );
+
+cron.schedule("00 00 1 * *", () => {
+  interest();
+});
 
 app.listen(PORT, () =>
   console.log(`Server running on port: http://localhost:${PORT}`)
